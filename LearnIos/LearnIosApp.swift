3394e5b61b76f69fe26146e286b51146d86e9b8a
lexynotfound <raihanardila22@gmail.com>
2025-02-08 08:54:41 +0700

    Initial Commit

---
+++ LearnIos/LearnIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnIosApp.swift
+//  LearnIos
+//
+//  Created by rai on 08/02/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearnIosTests/LearnIosTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnIosTests.swift
+//  LearnIosTests
+//
+//  Created by rai on 08/02/25.
+//
+
+import Testing
+@testable import LearnIos
+
+struct LearnIosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
